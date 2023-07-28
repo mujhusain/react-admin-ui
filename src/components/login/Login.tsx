be0b72490769daa86a75c57f575bde5bbf12a950
Mujaffar Husain <93472360+mujhusain@users.noreply.github.com>
2023-07-28 23:34:12 +0530

    login screen created

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,6 +1,52 @@
-import "./login.scss";
-const Login=()=>{
-    return <div className="login">Login</div>
-}
+import React, { useState } from "react";
+import "./Login.scss";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleLogin = () => {
+    // Implement login functionality here
+    // For simplicity, we'll just log the form data for now
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("Remember Me:", rememberMe);
+  };
+
+  return (
+      <div className="login-container">
+        <h1>Login</h1>
+        <div className="input-container">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="checkbox-container">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            checked={rememberMe}
+            onChange={(e) => setRememberMe(e.target.checked)}
+          />
+          <label htmlFor="rememberMe">Remember Me</label>
+        </div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+  );
+};
 
 export default Login;
